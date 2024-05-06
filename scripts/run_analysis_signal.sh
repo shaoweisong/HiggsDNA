ca44224adbaef574d4ea1ecb93787271e583d08f
shsong <shaowei.song@cern.ch>
2024-05-06 10:05:18 +0200

    SMZG

--- scripts/run_analysis_signal.sh
+++ scripts/run_analysis_signal.sh
@@ -1,4 +1,5 @@
 outdir="/afs/cern.ch/work/z/zewang/private/HZGamma/signal2"
 
 python scripts/run_analysis.py --sample_list "ggH_M125" --config "metadata/zgamma_signal.json" --log-level "DEBUG" --n_cores 5 --output_dir /eos/user/s/shsong/HiggsDNA/hzgtest --unretire_jobs --short --batch_system "local"
+python scripts/run_analysis.py --sample_list "ZGToLLG_01J_5f_lowMLL_lowGPt_TuneCP5_13TeV-amcatnloFXFX-pythia8"s --config "metadata/zgamma_signal.json" --log-level "DEBUG" --n_cores 5 --output_dir /eos/user/s/shsong/HiggsDNA/smzg --unretire_jobs --short --batch_system "local"
 #python scripts/convert_parquet_to_root.py --source $outdir/ggH_M125_2017/merged_nominal.parquet --target $outdir/merged.root --type mc --log DEBUG --process ggh --notag
